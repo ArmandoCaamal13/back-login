dbb120afe3dbbda7b098931a5905f5578b39a12f
LAPTOP-MT8OFFLO\Admin <yair@nubir.mx>
2022-05-20 16:03:15 -0500

    Initial commit

---
+++ src/main/java/login/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package login.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import login.models.ERole;
+import login.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
